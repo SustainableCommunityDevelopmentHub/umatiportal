5aa905ce484feb34eea928c2cab5e812c0e76dfe
G. Willson <greg@biomassiv.es>
2020-10-23 09:03:59 -0400

    removed unused api controllers and libs

--- controllers/api.js
+++ controllers/api.js
@@ -5,17 +5,12 @@ const { LastFmNode } = require('lastfm');
 const tumblr = require('tumblr.js');
 const GitHub = require('@octokit/rest');
 const Twit = require('twit');
-const stripe = require('stripe')(process.env.STRIPE_SKEY);
 const twilio = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 const clockwork = require('clockwork')({ key: process.env.CLOCKWORK_KEY });
-const paypal = require('paypal-rest-sdk');
 const lob = require('lob')(process.env.LOB_KEY);
-const ig = require('instagram-node').instagram();
 const axios = require('axios');
-const Quickbooks = require('node-quickbooks');
 const validator = require('validator');
 
-Quickbooks.setOauthVersion('2.0');
 
 /**
  * GET /api
@@ -166,19 +161,6 @@ exports.getGithub = async (req, res, next) => {
   }
 };
 
-exports.getQuickbooks = (req, res) => {
-  const token = req.user.tokens.find((token) => token.kind === 'quickbooks');
-
-  const qbo = new Quickbooks(process.env.QUICKBOOKS_CLIENT_ID, process.env.QUICKBOOKS_CLIENT_SECRET,
-    token.accessToken, false, req.user.quickbooks, true, false, null, '2.0', token.refreshToken);
-
-  qbo.findCustomers((_, customers) => {
-    res.render('api/quickbooks', {
-      title: 'Quickbooks API',
-      customers: customers.QueryResponse.Customer
-    });
-  });
-};
 
 /**
  * GET /api/nyt
